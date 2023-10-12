14e763d1aa845abed99d8405d7c8280af1fe53ff
Huang-qy-Chi <79854798+Huang-qy-Chi@users.noreply.github.com>
2023-10-12 16:56:34 +0800

    Update Gen_data.R

--- Gen_data.R
+++ Gen_data.R
@@ -1,26 +1,9 @@
 # Function of Data Genetate
 library(MASS)
 library(LaplacesDemon)
-# Case1: Vec(e^T)~N(0,In\otimes Sigma)
-Gen_data1 = function(n = 100, beta, Sigma0){
-  q = ncol(Sigma0)
-  In = diag(rep(1,n))
-  vecVar = kronecker(In,Sigma0)
-  VE = MASS::mvrnorm(n*q, mu = rep(0,n*q), Sigma = vecVar)#按行拉直后重组矩???
-  E = matrix(VE,nrow = n, ncol = q, byrow = T) #矩阵正态扰???
-  X = matrix(rnorm(n),ncol = 1)
-  Y = X[1,]%*%beta + E[1,]
-  for(i in 2:n){
-    Y = c(Y, X[i,]%*%beta + E[i,]) #按行拉直的Y
-  }
-  Y = matrix(Y, nrow = n, ncol = q, byrow = T)
-  Y_string = sapply(1:q, function(u){return(paste("Y", u, sep = ""))})
-  colnames(Y) <- Y_string
-  colnames(X) <- c("X")
-  return(list(Y = Y, X = X))
-}
 
 
+#normal
 library(LaplacesDemon)
 Gen_data = function(n = 100, beta, Sigma0){
   q = length(beta)
@@ -30,7 +13,7 @@ Gen_data = function(n = 100, beta, Sigma0){
   return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
 }
 
-
+#cauchy
 Gen_data2 = function(n = 100, beta, S, COR){
   library(LaplacesDemon)
   q = length(beta)
@@ -40,21 +23,27 @@ Gen_data2 = function(n = 100, beta, S, COR){
   return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
 }
 
-Gen_data3 = function(n = 100, beta, mu, Sigma1, Sigma2,ratio = 0.2){
+#mixnormal
+Gen_data3 = function(n = 100, beta, mu, Sigma1, Sigma2,ratio = 0.1){
   library(LaplacesDemon)
   q = as.numeric(ncol(Sigma1))
   X = rnorm(n)
-  w =runif(1)
-  if(w<1-ratio){
-    epsilon = rmvn(n, mu = rep(0,q), Sigma = Sigma1)
-  }else{
-    epsilon = rmvn(n, mu = mu, Sigma = Sigma2)
+  #epsilon = rmvn(n, mu = rep(0,q), Sigma = Sigma1)+rbind(matrix(rep(0,n*(1-ratio)*q),ncol=q),rmvn(n*ratio,mu,Sigma2))
+  #epsilon = 0.8*rmvn(n, mu = rep(0,q), Sigma = Sigma1)+0.2*rmvn(n, mu = mu, Sigma = Sigma2)
+  w = runif(n)
+  epsilon = matrix(rep(0,n*q),ncol = q)
+  for(j in 1:n){
+    if(w[j]<1-ratio){
+      epsilon[j,] = rmvn(1, mu = rep(0,q), Sigma = Sigma1)
+    }else{
+      epsilon[j,] = rmvn(1, mu = mu, Sigma = Sigma2)
+    }
   }
-    Y = X%*%t(beta) + epsilon
+  Y = X%*%t(beta) + epsilon
   return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
 }
 
-#Question
+#Question heteroscedasticity
 Gen_data4 = function(n = 100, beta, Sigma0){
   library(LaplacesDemon)
   q = as.numeric(ncol(Sigma0))
@@ -66,7 +55,11 @@ Gen_data4 = function(n = 100, beta, Sigma0){
   Y = X%*%t(beta) + epsilon
   return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
 }
+
+
+
 library(LaplacesDemon)
+# real heteroscedasticity
 Gen_data5 = function(n = 100, beta, Sigma0){
   q = as.numeric(ncol(Sigma0))
   X = rnorm(n)
@@ -77,35 +70,95 @@ Gen_data5 = function(n = 100, beta, Sigma0){
 
 
 
-
+#test for distribution of x
 Gen_data6 = function(n = 100, beta, Sigma0){
   q = length(beta)
   #X = rnorm(n,5,1)
   #X = rcauchy(n)
-  X = rexp(n)
+  X = rnorm(n)
   epsilon = rmvn(n, mu = rep(0,q), Sigma = Sigma0)
   Y = X%*%t(beta) + epsilon
   return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
 }
 
+#t distribution
+Gen_data7 = function(n = 100, beta, Sigma0, df = Inf){
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  epsilon = rmvt(n, mu = rep(0,q), S = Sigma0, df)
+  Y = X%*%t(beta) + epsilon
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
+
+#Multivarate laplace
+Gen_data8 = function(n = 100, beta, Sigma0){
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  epsilon = rmvl(n,mu = rep(0,q), Sigma = Sigma0)
+  Y = X%*%t(beta) + epsilon
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
+  
+
+#Multivariate log-Normal
+Gen_data9 = function(n = 100, beta, Sigma0){
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  epsilon = exp(rmvn(n,mu = rep(0,q), Sigma = Sigma0))
+  Y = X%*%t(beta) + epsilon
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
+
+Gen_data9X = function(n = 100, beta, Sigma0){
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  lep = exp(rnorm(n, 0, Sigma0))
+  for (j in 2:q){
+    lep = cbind(lep, exp(rnorm(n, 0, Sigma0)))
+  }
+  #epsilon = exp(rmvn(n,mu = rep(0,q), Sigma = Sigma0))
+  Y = X%*%t(beta) + lep
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
+#Gen_data9X(n=100, beta = c(1,0,1), Sigma0 = 2) 
 
 
+#g-h distribution
+library(gk)
 
+Gen_dataTgh = function(n = 100, beta, A, B, g=1, h=0.2){ #as the reviewer requested
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  epsilon = matrix(rep(0, n*q), ncol = q)
+  for(j in 1:q){
+    epsilon[,j] = rgh(n, A, B, g=1, h=0.2, c = 0.8, type = "tukey")
+  }
+  Y = X%*%t(beta) + epsilon
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
 
+#Gen_dataTgh(n=100, beta=c(1,0,0), A=0,B=1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Gen_dataGgh = function(n = 100, beta, A, B, g=1, h=0.2){ #as the reviewer requested
+  q = length(beta)
+  #X = rnorm(n,5,1)
+  #X = rcauchy(n)
+  X = rnorm(n)
+  epsilon = matrix(rep(0, n*q), ncol = q)
+  for(j in 1:q){
+    epsilon[,j] = rgh(n, A, B, g=1, h=0.2, c = 0.8, type = "generalised")
+  }
+  Y = X%*%t(beta) + epsilon
+  return(list(Y = matrix(Y,ncol = q), X = matrix(X,ncol = 1)))
+}
 
